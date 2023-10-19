953b49347959a325cd9adf578d39f9b2a19ee335
Sudheshna Jinna <sudheshnareddy1720@gmail.com>
2023-10-19 13:36:09 -0400

    first-commit

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Contributors: Sudheshna Jinna, Yashas Kuchimanchi</p>
+      <p className="icons">
+      </p>
+    </div>
+  )
+}
+
+export default Footer
